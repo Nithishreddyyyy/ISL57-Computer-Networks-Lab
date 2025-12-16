baa4261230956b03c208c72dd7a3f69d14ae400a
Nithish Reddy <nithishreddyb2@gmail.com>
2025-12-16 14:50:40 +0530

    major fix Q8

--- PART B/Quest8.c
+++ PART B/Quest8.c
@@ -8,45 +8,51 @@ typedef struct {
     int via[N];
 } Node;
 
-int main(){
+int main() {
     int cost[N][N] = {
-        {0, 2, INF, 1 , INF},
-        {2,   0,   3,   2,   INF},  // B
-        {INF, 3,   0,   4,   5},    // C
-        {1,   2,   4,   0,   3},    // D
-        {INF, INF, 5,   3,   0}     // E        
+        {0,   2,   INF, 1,   INF},
+        {2,   0,   3,   2,   INF},
+        {INF, 3,   0,   4,   5},
+        {1,   2,   4,   0,   3},
+        {INF, INF, 5,   3,   0}
     };
-
     Node node[N];
-    int i , j , k , updated;
-
-    for (int i = 0 ; i < N ; i++){
-        for (int j = 0 ; j < N ; j++){
+    int i, j, k, updated;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             node[i].dist[j] = cost[i][j];
-            node[i].via[j] = j;
+            if (cost[i][j] != INF && i != j)
+                node[i].via[j] = j;
+            else
+                node[i].via[j] = -1;
         }
     }
-
-    do{
+    do {
         updated = 0;
-        for(i =0 ; i<N ; i++){
-            for(j = 0 ; j < N ; j++){
-                for (k = 0 ; k < N ; k++){
-                    if(node[i].dist[j] > cost[i][k] + node[k].dist[j]){
-                        node[i].dist[j] = cost[i][k] + node[k].dist[j];
-                        node[i].via[j] = k;
-                        updated=1;
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                for (k = 0; k < N; k++) {
+                    if (node[i].dist[k] != INF &&
+                        node[k].dist[j] != INF &&
+                        node[i].dist[j] > node[i].dist[k] + node[k].dist[j]) {
+
+                        node[i].dist[j] = node[i].dist[k] + node[k].dist[j];
+                        node[i].via[j] = node[i].via[k];
+                        updated = 1;
                     }
                 }
             }
         }
-    }while(updated);
+    } while (updated);
 
-    for(i= 0 ; i <N ; i++){
+    for (i = 0; i < N; i++) {
         printf("\nRouting Table for Router %c:\n", 'A' + i);
         printf("Dest\tNextHop\tDist\n");
-        for(j = 0 ; j < N ; j++){
-            printf("%c\t%c\t%d\n",'A'+j,'A' + node[i].via[j], node[i].dist[j]);
+        for (j = 0; j < N; j++) {
+            if (node[i].via[j] != -1)
+                printf("%c\t%c\t%d\n", 'A'+j, 'A'+node[i].via[j], node[i].dist[j]);
+            else
+                printf("%c\t-\t%d\n", 'A'+j, node[i].dist[j]);
         }
     }
     return 0;
